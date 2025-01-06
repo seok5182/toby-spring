4454e34fead372253afea4eb25a8d0a7048764f1
seok5182 <seok5182@naver.com>
2025-01-06 23:55:08 +0900

    1.3.3 관계설정 책임의 분리

---
+++ src/test/java/toby/spring/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,32 @@
+package toby.spring.springbook.user.dao;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import toby.spring.springbook.user.domain.User;
+
+@SpringBootTest
+class UserDaoTest {
+
+	@Test
+	void test() throws SQLException, ClassNotFoundException {
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+
+		UserDao dao = new UserDao(connectionMaker);
+
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+}
