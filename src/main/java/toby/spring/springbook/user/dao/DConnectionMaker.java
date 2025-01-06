445473a6cb091b8c7d8426b0d3a0731d90b82f35
seok5182 <seok5182@naver.com>
2025-01-06 22:31:44 +0900

    1.3.2 인터페이스의 도입

---
+++ src/main/java/toby/spring/springbook/user/dao/DConnectionMaker.java
@@ -0,0 +1,16 @@
+package toby.spring.springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker {
+
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException, SQLException {
+		// D사의 독자적인 방법으로 Connection을 생성하는 코드
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby", "root", "Skdud12!@");
+		return c;
+	}
+}
