b932a2f32d2b3ce8e8b000368ee9f2a66dd7202f
seok5182 <seok5182@naver.com>
2025-01-06 20:10:17 +0900

    1.1.3 DAO 테스트 코드

--- src/test/java/toby/spring/ApplicationTests.java
+++ src/test/java/toby/spring/ApplicationTests.java
@@ -1,7 +1,10 @@
 package toby.spring;
 
+import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import toby.spring.springbook.user.dao.UserDao;
+import toby.spring.springbook.user.domain.User;
 
 @SpringBootTest
 class ApplicationTests {
@@ -10,4 +13,23 @@ class ApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void test() throws SQLException, ClassNotFoundException {
+		UserDao dao = new UserDao();
+
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+
+		System.out.println(user2.getId() + " 조회 성공");
+	}
 }
