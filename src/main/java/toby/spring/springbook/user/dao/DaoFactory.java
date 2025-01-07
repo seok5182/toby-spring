5dbba4ad1b291227cbd2fa2804f62a43cda689b4
seok5182 <seok5182@naver.com>
2025-01-07 11:47:59 +0900

    1.4.1 오브젝트 팩토리

---
+++ src/main/java/toby/spring/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package toby.spring.springbook.user.dao;
+
+public class DaoFactory {
+
+	public UserDao userDao() {
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+		UserDao userDao = new UserDao(connectionMaker);
+		return userDao;
+	}
+}
--- src/test/java/toby/spring/springbook/user/dao/UserDaoTest.java
+++ src/test/java/toby/spring/springbook/user/dao/UserDaoTest.java
@@ -8,11 +8,33 @@ import toby.spring.springbook.user.domain.User;
 @SpringBootTest
 class UserDaoTest {
 
+	// 1.3.3 테스트
 	@Test
-	void test() throws SQLException, ClassNotFoundException {
+	void test1() throws SQLException, ClassNotFoundException {
 		ConnectionMaker connectionMaker = new DConnectionMaker();
 
 		UserDao dao = new UserDao(connectionMaker);
+
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+
+	// 1.4.1 테스트
+	@Test
+	void test2() throws SQLException, ClassNotFoundException {
+		UserDao dao = new DaoFactory().userDao();
 
 		User user = new User();
 		user.setId("whiteship");
