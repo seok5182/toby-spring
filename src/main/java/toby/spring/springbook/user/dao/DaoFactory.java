d1d1c3ab54ea2ad12cb3a9122673c7e6e956f3cf
seok5182 <seok5182@naver.com>
2025-01-07 11:56:50 +0900

    1.4.2 오브젝트 팩토리의 활용

--- src/main/java/toby/spring/springbook/user/dao/DaoFactory.java
+++ src/main/java/toby/spring/springbook/user/dao/DaoFactory.java
@@ -3,8 +3,11 @@ package toby.spring.springbook.user.dao;
 public class DaoFactory {
 
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		return userDao;
+		return new UserDao(connectionMaker());
+	}
+
+	// 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
 	}
 }
