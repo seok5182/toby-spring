a0ca054d52c01632113b356507b6f8cceaf84a22
seok5182 <seok5182@naver.com>
2025-01-07 14:09:42 +0900

    1.5.1 오브젝트 팩토리를 이용한 스프링 IoC

--- src/main/java/toby/spring/springbook/user/dao/DaoFactory.java
+++ src/main/java/toby/spring/springbook/user/dao/DaoFactory.java
@@ -1,12 +1,18 @@
 package toby.spring.springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 뜻
 public class DaoFactory {
 
+	@Bean // 오브젝트 생성을 담당하는 IoC 메소드라는 뜻
 	public UserDao userDao() {
 		return new UserDao(connectionMaker());
 	}
 
 	// 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
--- src/test/java/toby/spring/springbook/user/dao/UserDaoTest.java
+++ src/test/java/toby/spring/springbook/user/dao/UserDaoTest.java
@@ -3,6 +3,8 @@ package toby.spring.springbook.user.dao;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import toby.spring.springbook.user.domain.User;
 
 @SpringBootTest
@@ -35,6 +37,28 @@ class UserDaoTest {
 	@Test
 	void test2() throws SQLException, ClassNotFoundException {
 		UserDao dao = new DaoFactory().userDao();
+
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+
+		dao.add(user);
+
+		System.out.println(user.getId() + " 등록 성공");
+
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+
+	// 1.5.1 테스트
+	@Test
+	void test3() throws SQLException, ClassNotFoundException {
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 
 		User user = new User();
 		user.setId("whiteship");
