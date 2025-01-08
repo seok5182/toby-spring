5d504d448a0df38056758421f2b3f93dee1a4bbc
seok5182 <seok5182@naver.com>
2025-01-08 18:17:24 +0900

    1.7.5 메소드를 이용한 의존관계 주입(수정자)

--- src/main/java/toby/spring/springbook/user/dao/CountingDaoFactory.java
+++ src/main/java/toby/spring/springbook/user/dao/CountingDaoFactory.java
@@ -6,9 +6,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CountingDaoFactory {
 
+	// 생성자 DI를 사용하는 팩토리 메소드
+	/*
 	@Bean
 	public UserDao userDao() {
 		return new UserDao(connectionMaker());
+	}
+	*/
+
+	// 수정자 메소드 DI를 사용하는 팩토리 메소드
+	@Bean
+	public UserDao userDao() {
+		UserDao userDao = new UserDao();
+		userDao.setConnectionMaker(connectionMaker());
+		return userDao;
 	}
 
 	@Bean
